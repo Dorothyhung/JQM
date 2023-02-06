794da4feaa1c13c2c94cffd409408829fd9669b4
Dorothyhung <dorothyh0602@gmail.com>
2023-02-05 19:56:55 -0800

    add search function and data validation in form

--- main.js
+++ main.js
@@ -6,22 +6,30 @@ var Song = function (songname, artist, album) {
     this.album=album;
 }
 
-let list = document.getElementById("myList")
+let list = document.getElementById("myList");
+let list2 = document.getElementById("myList");
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    //Add to Library
     this.getElementById("add").addEventListener("click", function() {
         let songname = document.getElementById("songname").value
         let artist = document.getElementById("artist").value
         let album = document.getElementById("album").value
-        var newSong = new Song(songname, artist, album);
-        songList.push(newSong);
-        console.log(songList);
-        document.getElementById("added").value = (songname + ' by ' + artist + ' successfully added to Library!');
-        document.getElementById("songname").value = "";
-        document.getElementById("artist").value = "";
-        document.getElementById("album").value = "";
+        if((songname == "") || (artist == "") || (album == "")) {
+            alert("Please enter text in all fields");
+        } else {
+            var newSong = new Song(songname, artist, album);
+            songList.push(newSong);
+            console.log(songList);
+            document.getElementById("added").value = (songname + ' by ' + artist + ' successfully added to Library!');
+            document.getElementById("songname").value = "";
+            document.getElementById("artist").value = "";
+            document.getElementById("album").value = "";
+        }
+        
     })
 
+    //View Library
     this.getElementById("viewList").addEventListener("click", function() {
         document.getElementById("myList").innerHTML = "";
         document.getElementById("added").value = '';
@@ -31,6 +39,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
             console.log(songList[i].songname);
             li.innerText = songList[i].songname + " by " + songList[i].artist + " in " + songList[i].album;
             list.appendChild(li);
+        }
+    })
+
+    //Search Library
+    this.getElementById("searchList").addEventListener("click", function() {
+        document.getElementById("myList2").innerHTML = "";
+        document.getElementById("added").value = '';
+        list2 = document.getElementById("myList2");
+        for (i = 0; i < songList.length; i++) {
+            let li = document.createElement("li");
+            console.log(songList[i].songname);
+            li.innerText = songList[i].songname + " by " + songList[i].artist + " in " + songList[i].album;
+            list2.appendChild(li);
         }
     })
 })
